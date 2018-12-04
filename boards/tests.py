a22a1a24e913bfb9302746974a37d64c325b015b
SabitDeepto <deepto69@gmail.com>
2018-12-04 12:19:18 +0600

    1st test case

--- boards/tests.py
+++ boards/tests.py
@@ -1,9 +1,8 @@
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
-class HomeTest(TestCase):
-	def test_home_view_status(self):
-		url = reverse('home')
-		response = self.client.get(url)
-		self.assertEquals(response.status_code,200)
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
