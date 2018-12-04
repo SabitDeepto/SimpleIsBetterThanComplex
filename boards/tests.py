81818fd3c18cf32f72de1d4d482b05d9b647f856
SabitDeepto <deepto69@gmail.com>
2018-12-04 13:38:40 +0600

    2nd test case

--- boards/tests.py
+++ boards/tests.py
@@ -1,8 +1,14 @@
 from django.urls import reverse
 from django.test import TestCase
+from django.urls import resolve
+from .views import home
 
-class HomeTests(TestCase):
-    def test_home_view_status_code(self):
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+class TestHome(TestCase):
+	def checkHome(self):
+		url = reverse('home')
+		response = self.client.get(url)
+		self.assertEquals(response.status_code, 200)
+
+	def resolve_view_for_home_url(self):
+		view = resolve("/home")
+		self.assertEquals(views.func, home)
